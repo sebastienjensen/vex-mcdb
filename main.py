6c70b10af490db579a206d12e3b38195710ad038
sebastienjensen <160079243+sebastienjensen@users.noreply.github.com>
2025-02-06 22:45:41 +0000

    Use config.json, refresh from RE API

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Import libraries, modules
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-import subprocess
+import fetch
+import database
+import json
+
+# Import configuration
+with open('config.json') as configFile:
+    config = json.load(configFile)
 
 # Initialise Flask app
 app = Flask(__name__)
@@ -9,12 +15,77 @@ CORS(app)
 
 # ROUTES
 
-# Index, commit ID (short)
-@app.route('/')
+# Index
+@app.route("/")
 def index():
-    commitId = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode('utf-8')
-    return f'vex-mcdb {commitId}'
+    return "vex-mcdb"
+
+# Refresh data by region/country
+@app.route("/refresh/<string:target>")
+def refresh(target):
+    # Fetch and submit event data for insertion
+    if target == "event":
+        connection = database.connect(config["database"])
+        events = fetch.fetch(config, "events", {"region": config["region"], "season[]": config["season"]})
+        database.insert("event", events, connection)
+        connection.close()
+        return f"Refreshed {len(events)} events"
+    # Fetch and submit team data for insertion
+    elif target == "team":
+        connection = database.connect(config["database"])
+        teams = fetch.fetch(config, "teams", {"country": config["country"], "program[]": config["program"]})
+        database.insert("team", teams, connection)
+        connection.close()
+        return f"Refreshed {len(teams)} teams"
+    # Fetch and submit award data for insertion
+    # This can only be fetched after events have been fetched
+    elif target == "award":
+        connection = database.connect(config["database"])
+        # Retrieve list of known events
+        cursor = connection.cursor()
+        cursor.execute("SELECT event_id FROM event")
+        events = cursor.fetchall()
+        # Fetch and insert awards from all known events
+        for event in events:
+            awards = fetch.fetch(config, f"events/{event[0]}/awards", {})
+            database.insert("award", awards, connection)
+        cursor.close()
+        connection.close()
+        return f"Refreshed awards from {len(events)} events"
+    elif target == "match":
+        connection = database.connect(config["database"])
+        # Retrieve list of known events
+        cursor = connection.cursor()
+        cursor.execute("SELECT event_id, event_divisions FROM event")
+        events = cursor.fetchall()
+        # Fetch and insert matches from all known events
+        for event, divisions in events:
+            for division in range(1, divisions + 1):
+                matches = fetch.fetch(config, f"events/{event}/divisions/{division}/matches", {})
+                database.insert("match", matches, connection)
+        cursor.close()
+        connection.close()
+        return f"Refreshed matches from {len(events)} events"
+    
+# Return team IDs from team numbers and program
+@app.route("/teams/<int:program>/<string:team1>/<string:team2>/<string:team3>/<string:team4>")
+def retrieveID(program, team1, team2, team3, team4):
+    connection = database.connect(config["database"])
+    cursor = connection.cursor()
+    response = {
+        1: database.team_ids(program, team1, cursor),
+        2: database.team_ids(program, team2, cursor),
+        3: database.team_ids(program, team3, cursor),
+        4: database.team_ids(program, team4, cursor)
+    }
+    cursor.close()
+    connection.close()
+    return jsonify(response)
 
 # Run app
 if __name__ == "__main__":
+    connection = database.connect(config["database"])
+    # Initialize database if it doesn't exist
+    database.onStart(connection)
+    connection.close()
     app.run(host="0.0.0.0", debug=True)
