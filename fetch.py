80456aa61f55e825f86c1ac5ec406094cabba01a
sebastienjensen <160079243+sebastienjensen@users.noreply.github.com>
2025-02-06 22:45:17 +0000

    Access token from config.json

--- fetch.py
+++ fetch.py
@@ -1,33 +1,29 @@
-import os
 import requests
 import time
-from dotenv import load_dotenv
 
 root = 'https://www.robotevents.com/api/v2'
 
-# HTTP header
-load_dotenv()
-headers = {
-    'accept': 'application/json',
-    'Authorization': 'Bearer ' + os.environ.get('TOKEN')
-}
-
 # Standard request function
-def fetch(target, params):
+def fetch(config, target, params):
+    # HTTP header
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer " + config["token"]
+    }
     page = 1
     output = []
 
     # Provide for iteration through pages, if applicable
     while True:
-        suffix = f'/{target}?per_page=250&page={str(page)}'
+        suffix = f"/{target}?per_page=250&page={str(page)}"
         endpoint = root + suffix
         response = requests.get(endpoint, headers=headers, params=params).json()
-        output.extend(response['data'])
+        output.extend(response["data"])
 
         if page >= response["meta"]["last_page"]:
             break
 
         page += 1
-        time.sleep(1)
+        time.sleep(page)
     
     return output
