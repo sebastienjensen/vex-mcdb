41c12611ea8f42ce6c1bc084152d23076618968b
sebastienjensen <160079243+sebastienjensen@users.noreply.github.com>
2025-02-08 12:06:25 +0000

    Hide cards for teams not in a match

--- client/main.js
+++ client/main.js
@@ -21,6 +21,29 @@ async function getEvent() {
     refreshMatches();
 }
 
+// Hide cards for teams not playing
+function hideCards(mode) {
+    if (mode == 'v5') {
+        document.getElementById('iq').classList.add('hidden');
+        document.getElementById('iq1').classList.add('hidden');
+        document.getElementById('iq2').classList.add('hidden');
+        document.getElementById('v5').classList.remove('hidden');
+        document.getElementById('red1').classList.remove('hidden');
+        document.getElementById('red2').classList.remove('hidden');
+        document.getElementById('blue1').classList.remove('hidden');
+        document.getElementById('blue2').classList.remove('hidden');
+    } else {
+        document.getElementById('v5').classList.add('hidden');
+        document.getElementById('red1').classList.add('hidden');
+        document.getElementById('red2').classList.add('hidden');
+        document.getElementById('blue1').classList.add('hidden');
+        document.getElementById('blue2').classList.add('hidden');
+        document.getElementById('iq').classList.remove('hidden');
+        document.getElementById('iq1').classList.remove('hidden');
+        document.getElementById('iq2').classList.remove('hidden');
+    }
+}
+
 // Event listeners
 document.getElementById('theme').addEventListener('click', theme);
 document.getElementById('refresh').addEventListener('click', getEvent);
